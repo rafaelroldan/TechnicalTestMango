36417dfb970733c24610e756716ef14a663a89f9
rafaelroldan <rafael.roldan@sngular.com>
2024-04-15 13:02:26 +0200

    build(Base): Multimodule structure

---
+++ domain/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "com.rafaelroldan.domain"
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,7 @@ dependencyResolutionManagement {
 
 rootProject.name = "TechnicalTestMango"
 include(":app")
- 
+include(":data")
+include(":domain")
+include(":core")
+include(":features")
